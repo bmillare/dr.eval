876a2df1982ae4f3921d6dd3e15275f9bc898ce5
Brent Millare <brent.millare@gmail.com>
2013-12-04 16:08:02 -0500

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject dr.eval "0.2.0"
-  :profiles {:ring {:dependencies [[ring/ring-jetty-adapter "1.1.8"]
-                                   [ring/ring-devel "1.1.8"]
+  :profiles {:ring {:dependencies [[ring/ring-jetty-adapter "1.2.1"]
+                                   [ring/ring-devel "1.2.1"]
+                                   [ring/ring-core "1.2.1"]
                                    [ring-cors/ring-cors "0.1.0"]]}})
